f775f0fc41372cec2fc0b19b98beabab014cce01
Ruben Romero Montes <rromerom@redhat.com>
2020-05-11 03:30:11 +0200

    [KOGITO-1248] Add casemgmt tests (#467)
    * [KOGITO-1248] Add casemgmt tests
    
    Signed-off-by: ruromero <rromerom@redhat.com>
    
    * [KOGITO-1248] Create Metadata interface
    
    Signed-off-by: ruromero <rromerom@redhat.com>
    
    * [KOGITO-1248] Use generics in Visitors
    
    Signed-off-by: ruromero <rromerom@redhat.com>
    
    * [KOGITO-1248] Use missing generics in ProcessVisitors
    
    Signed-off-by: ruromero <rromerom@redhat.com>
    
    * Update jbpm/jbpm-flow-builder/src/main/java/org/jbpm/compiler/canonical/DynamicNodeVisitor.java
    
    Co-authored-by: Cristiano Nicolai <cristiano.nicolai@gmail.com>
    
    * Update jbpm/jbpm-flow-builder/src/main/java/org/jbpm/compiler/canonical/MilestoneNodeVisitor.java
    
    Co-authored-by: Cristiano Nicolai <cristiano.nicolai@gmail.com>
    
    * [KOGITO-1248] Rename class
    
    Signed-off-by: ruromero <rromerom@redhat.com>
    
    * [KOGITO-1248] Remove bpmn-cm extension and add missing test class
    
    Signed-off-by: ruromero <rromerom@redhat.com>
    
    * [KOGITO-1248] Remove code duplications and code smells
    
    Signed-off-by: ruromero <rromerom@redhat.com>
    
    Co-authored-by: Cristiano Nicolai <cristiano.nicolai@gmail.com>

--- jbpm-flow/src/main/java/org/jbpm/ruleflow/core/factory/StateNodeFactory.java
+++ jbpm-flow/src/main/java/org/jbpm/ruleflow/core/factory/StateNodeFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,26 +20,31 @@ import org.jbpm.process.core.context.exception.ExceptionHandler;
 import org.jbpm.process.core.datatype.DataType;
 import org.jbpm.ruleflow.core.RuleFlowNodeContainerFactory;
 import org.jbpm.workflow.core.NodeContainer;
+import org.jbpm.workflow.core.impl.ConnectionRef;
+import org.jbpm.workflow.core.impl.ConstraintImpl;
 import org.jbpm.workflow.core.node.CompositeContextNode;
-import org.jbpm.workflow.core.node.DynamicNode;
+import org.jbpm.workflow.core.node.StateNode;
 
-public class DynamicNodeFactory extends CompositeContextNodeFactory {
+import static org.jbpm.workflow.core.Node.CONNECTION_DEFAULT_TYPE;
 
-	public static final String METHOD_LANGUAGE = "language";
-	public static final String METHOD_ACTIVATION_EXPRESSION = "activationExpression";
-	public static final String METHOD_COMPLETION_EXPRESSION = "completionExpression";
+/**
+ *
+ */
+public class StateNodeFactory extends CompositeContextNodeFactory {
 
-    public DynamicNodeFactory(RuleFlowNodeContainerFactory nodeContainerFactory, NodeContainer nodeContainer, long id) {
+    public static final String METHOD_CONSTRAINT = "constraint";
+
+    public StateNodeFactory(RuleFlowNodeContainerFactory nodeContainerFactory, NodeContainer nodeContainer, long id) {
         super(nodeContainerFactory, nodeContainer, id);
     }
 
     @Override
     protected CompositeContextNode createNode() {
-        return new DynamicNode();
+        return new StateNode();
     }
 
-    protected DynamicNode getDynamicNode() {
-        return (DynamicNode) getNodeContainer();
+    protected StateNode getStateNode() {
+        return (StateNode) getNodeContainer();
     }
 
     @Override
@@ -48,65 +53,57 @@ public class DynamicNodeFactory extends CompositeContextNodeFactory {
     }
 
     @Override
-    public DynamicNodeFactory variable(String name, DataType type) {
+    public StateNodeFactory variable(String name, DataType type) {
         super.variable(name, type);
         return this;
     }
 
     @Override
-    public DynamicNodeFactory variable(String name, DataType type, Object value) {
+    public StateNodeFactory variable(String name, DataType type, Object value) {
         super.variable(name, type, value);
         return this;
     }
 
     @Override
-    public DynamicNodeFactory exceptionHandler(String exception, ExceptionHandler exceptionHandler) {
+    public StateNodeFactory exceptionHandler(String exception, ExceptionHandler exceptionHandler) {
         super.exceptionHandler(exception, exceptionHandler);
         return this;
     }
 
     @Override
-    public DynamicNodeFactory exceptionHandler(String exception, String dialect, String action) {
+    public StateNodeFactory exceptionHandler(String exception, String dialect, String action) {
         super.exceptionHandler(exception, dialect, action);
         return this;
     }
 
     @Override
-    public DynamicNodeFactory autoComplete(boolean autoComplete) {
+    public StateNodeFactory autoComplete(boolean autoComplete) {
         super.autoComplete(autoComplete);
         return this;
     }
 
     @Override
-    public DynamicNodeFactory linkIncomingConnections(long nodeId) {
+    public StateNodeFactory linkIncomingConnections(long nodeId) {
         super.linkIncomingConnections(nodeId);
         return this;
     }
 
     @Override
-    public DynamicNodeFactory linkOutgoingConnections(long nodeId) {
+    public StateNodeFactory linkOutgoingConnections(long nodeId) {
         super.linkOutgoingConnections(nodeId);
         return this;
     }
 
-    @Override
-    public DynamicNodeFactory metaData(String name, Object value) {
-        super.metaData(name, value);
-        return this;
-    }
-
-    public DynamicNodeFactory language(String language) {
-        getDynamicNode().setLanguage(language);
-        return this;
-    }
-
-    public DynamicNodeFactory activationExpression(String activationExpression) {
-        getDynamicNode().setActivationExpression(activationExpression);
-        return this;
-    }
-
-    public DynamicNodeFactory completionExpression(String completionExpression) {
-        getDynamicNode().setCompletionExpression(completionExpression);
+    public StateNodeFactory constraint(String connectionId, long nodeId, String type, String dialect, String constraint, int priority) {
+        ConstraintImpl constraintImpl = new ConstraintImpl();
+        constraintImpl.setName(connectionId);
+        constraintImpl.setType(type);
+        constraintImpl.setDialect(dialect);
+        constraintImpl.setConstraint(constraint);
+        constraintImpl.setPriority(priority);
+        getStateNode().addConstraint(
+                new ConnectionRef(connectionId, nodeId, CONNECTION_DEFAULT_TYPE),
+                constraintImpl);
         return this;
     }
 }
